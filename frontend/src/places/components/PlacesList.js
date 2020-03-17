bae3ec55ea97ba256cabfbc1832c2b5f9d3782ab
DasH <fxfddfno@gmail.com>
2020-03-17 18:18:07 +0200

    start with show user places

---
+++ frontend/src/places/components/PlacesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Card from "../../shared/components/UIElements/Card";
+
+import "./PlacesList.css";
+
+const PlacesList = ({ items })=>{
+    return(
+        items.length > 0 ? (
+            <ul className="place-list">
+                {
+                    items.map(
+                        place => <PlaceItem key={place._id} place={place}/>
+                    )
+                }
+            </ul>
+        ) 
+        : 
+        (
+            <div className="place-list center">
+                <Card>
+                    <h2> No places found. Maybe create one? </h2>
+                    <button> Share Places </button>
+                </Card>
+            </div>
+        )
+    )
+}
