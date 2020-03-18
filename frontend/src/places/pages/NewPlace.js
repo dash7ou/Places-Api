b55cb361076a9743e6ee3c17a2f1e0574121abbd
DasH <fxfddfno@gmail.com>
2020-03-18 21:06:22 +0200

    start with add new place page

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+
+import "./NewPlace.css"
+
 const NewPlace = ()=>{
-    return <div>new place</div>
+    return (
+        <form className="place-form">
+            <Input type="text" label="Title" element="input"/>
+        </form>
+    )
 }
 
 export default NewPlace;
