7677faf0015ebf04163026c9e36335211f528c7a
DasH <fxfddfno@gmail.com>
2020-03-20 17:59:22 +0200

    confirm delete

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -9,8 +9,17 @@ import "./PlaceItem.css"
 
 const PlaceItem = ({ place: {_id, imageUrl, title, description, address, creator, location }})=>{
     const [showMap, changeStateShow] = useState(false);
+    const [showConfirm , changeConfirmState ] = useState(false);
+
     const onChangeStateShow = ()=>{
         changeStateShow(!showMap)
+    }
+    const onCahngeStateShowConfirm = ()=>{
+        changeConfirmState(!showConfirm)
+    }
+    const confirmDeleteHandler = ()=>{
+        console.log("delete")
+        onCahngeStateShowConfirm()
     }
     return (
         <Fragment>
@@ -26,6 +35,24 @@ const PlaceItem = ({ place: {_id, imageUrl, title, description, address, creator
                     <Map center={location} zoom={16}/>
                 </div>
             </Modal>
+            <Modal
+                show={showConfirm}
+                onCancel={onCahngeStateShowConfirm}
+                header="Are you sure ?"
+                footerClass="place-item__modal-actions"
+                footer={
+                    <Fragment>
+                        <Button inverse onClick={onCahngeStateShowConfirm}> Cancel </Button>
+                        <Button danger onClick={confirmDeleteHandler}> Delete </Button>
+                    </Fragment>
+                }
+
+
+            >
+                <p>
+                    Do you want to proceed and delete this place? Please note that it can't be undone.
+                </p>
+            </Modal>
             <li className="place-item">
                 <Card className="place-item__content">
                     <div className="place-item__image">
@@ -39,7 +66,7 @@ const PlaceItem = ({ place: {_id, imageUrl, title, description, address, creator
                     <div className="place-item__actions">
                         <Button inverse onClick={onChangeStateShow}>View On Map</Button>
                         <Button to={`/places/${_id}`}>EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button danger onClick={onCahngeStateShowConfirm}>DELETE</Button>
                     </div>
                 </Card>
             </li>
