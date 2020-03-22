2a14df8b09afe33e34350dd2845ed97c76eba3fd
DasH <fxfddfno@gmail.com>
2020-03-22 11:42:53 +0200

    middleware handling error

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use("/api/v1/places");
+
+app.use((error, req, res, next)=>{
+    if(!res.headerSent){
+        return next(error)
+    }
+
+    res.status(error.code || 500);
+    res.send({
+        message: error.message || 'An unknown error occurred!'
+    })
+})
+
 app.listen(5000, ()=>{
     console.log("server run successful.. port 5000")
 })
