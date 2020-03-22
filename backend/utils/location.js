6f2e6c4acb72d9102ee7959cfd82a0d802b6dfa5
DasH <fxfddfno@gmail.com>
2020-03-22 13:42:44 +0200

    get location function

---
+++ backend/utils/location.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = "AIzaSyCOwRmPeaTFXyEdxokz3rgVbEgbv3hHabQ";
+
+async function getCoordsForAddress( address ){
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+    const { data } = response;
+    if(!data || data.status === 'ZERO_RESULTS'){
+        const errors = new HttpError("Could not found location for specified address.", 422)
+        throw errors;
+    }
+
+    const coordinates = data.results[0].geometry.location;
+    return coordinates;
+}
+
+
+module.exports = getCoordsForAddress;
