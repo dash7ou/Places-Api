951605503b29f0b4a4e443a1da22bcecfd3a51ed
DasH <fxfddfno@gmail.com>
2020-03-24 21:44:40 +0200

    update place

--- backend/controllers/place.js
+++ backend/controllers/place.js
@@ -60,10 +60,45 @@ exports.createPlace = async ( req , res , next) =>{
     }
     res.status(200).send(place)
 }
-exports.updatePalace = (req ,res, next)=>{
+exports.updatePalace = async (req ,res, next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         throw new HttpError("Invalid inputs passedm please check your data", 422)
     }
+
+    const {
+        body:{
+            title,
+            description
+        },
+        params:{
+            id: placeId
+        }
+    } = req;
+
+    let place;
+    try{
+        place = await Place.findOne({
+            _id: placeId
+        });
+    } catch(err){
+        const error = new HttpError("Error in update place please try again.", 500);
+        return next(error);
+    }
+
+
+    if(!place) next(new HttpError("There is no place with this id", 404));
+
+    place.title= title;
+    place.description = description;
+
+    try{
+        await place.save();
+    }catch(err){
+        console.log(err)
+        const error = new HttpError("Error in update place please try again.", 500);
+        return next(error);
+    }
+    res.status(200).send(place)
 }
 exports.deletePlace = (req, res, next)=>{}
