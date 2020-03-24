9583fa0dcc2ec7290f3bbcfcc869e2adb4b04c0d
DasH <fxfddfno@gmail.com>
2020-03-24 21:57:59 +0200

    create user model

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    places:[
+        {
+            type: String
+        }
+    ]
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
