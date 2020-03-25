126933d7098462ae0f7c9bade594f32a99592f84
DasH <fxfddfno@gmail.com>
2020-03-25 23:03:54 +0200

    http hook

---
+++ frontend/src/shared/hooks/http-hook.js
@@ -0,0 +1,49 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+export const useHttpClient = ()=>{
+    const [ isLoading , setIsLoading ] = useState(false);
+    const [ error , setError ]= useState(null);
+
+    const activeHttpRequests = useRef([]);
+
+    const sendRequest = useCallback( 
+        async (url , method='GET', body=null, headers={})=>{
+            try {
+                setIsLoading(true);
+                const httpAbortCtrl = new AbortController();
+                activeHttpRequests.current.push(httpAbortCtrl);
+
+                const response = await fetch(url, {
+                    method,
+                    body,
+                    headers,
+                    signal: httpAbortCtrl.signal
+                });
+        
+                const data = await response.json();
+                if(!response.ok){
+                    throw new Error(data.message);
+                }
+                setIsLoading(false);
+                return data
+            } catch (error) {
+                setError(error.message);
+                setIsLoading(false)
+            }
+
+        },[]
+    );
+
+    const clearError = ()=>{
+        setError(null);
+    }
+
+    useEffect(()=>{
+        return ()=>{
+            activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort())
+        }
+    }, [])
+
+    return [ isLoading, error, sendRequest, clearError];
+};
+
