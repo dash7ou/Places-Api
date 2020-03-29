43866d532c65bc615c1e80e1f802ed5564424ea0
DasH <fxfddfno@gmail.com>
2020-03-29 23:55:15 +0300

    image upload

--- frontend/src/shared/components/FormElements/ImageUpload.js
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+import Button from "./Button";
+
 import "./ImageUpload";
 
 
 const ImageUpload = ()=>{
     return(
-        <div className="form-control"></div>
+        <div className="form-control">
+            <input id={id} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg"/>
+            <div className={`image-upload ${center && 'center'}`}>
+                <div className="image-upload__preview">
+                    <img src="" alt="Preview" />
+                </div>
+                <Button type="button"> PICK IMAGE</Button>
+            </div>
+        </div>
     )
 }
 
