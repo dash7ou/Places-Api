b43c272a72f900fae33c25393057a5015aee89ae
DasH <fxfddfno@gmail.com>
2020-03-31 21:08:52 +0300

    automaticly logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,35 @@ import { AuthContext } from "./shared/context/auth-context";
 
 import './App.css';
 
+let logoutTimer ;
 const App = ()=> {
   const [token, setToken ] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExporationDate] = useState(null)
 
   const login = useCallback((uid, token, expirationDate)=>{
     setUserId(uid);
     setToken(token);
-    const tokenExpirationDate = expirationDate ||  new Date(new Data().getTime() + 1000*60*60);
+    const tokenExpirationDate = expirationDate ||  new Date(new Date().getTime() + 1000*60*60);
+    setTokenExporationDate(tokenExpirationDate);
     localStorage.setItem("userData", JSON.stringify({ userId: uid, token , expiration: tokenExpirationDate.toISOString() }));
   }, []);
 
   const logout = useCallback(()=>{
     setToken(null);
     setUserId(null)
+    setTokenExporationDate(null);
     localStorage.removeItem("userData");
   }, [])
 
+  useEffect(()=>{
+    if(token && tokenExpirationDate){
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer =  setTimeout(logout ,remainingTime )
+    }else{
+      clearTimeout(logoutTimer);
+    }
+  }, [token , logout, tokenExpirationDate])
 
   useEffect(()=>{
     const storedData = JSON.parse(localStorage.getItem("userData"));
